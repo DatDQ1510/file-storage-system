a19614e65b0120fda44c81cb3f3ff501972b24b7
DatDQ5275 <datdinh5275@gmail.com>
2026-03-26 00:41:27 +0700

    Create Config Redis

--- src/main/java/com/java/file_storage_system/externalService/MailService.java
+++ src/main/java/com/java/file_storage_system/externalService/MailService.java
@@ -2,6 +2,7 @@ package com.java.file_storage_system.externalService;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,7 +15,10 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender; // Spring tự động lấy cấu hình từ properties nạp vào đây
 
-//    @Scheduled(fixedRate = 5000)
+    @Autowired
+    private RedisTemplate template;
+
+    @Scheduled(fixedRate = 5000)
     public void guiEmailDonGian() {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo("bhhoang1203@gmail.com");
@@ -22,5 +26,11 @@ public class MailService {
         message.setText("Nội dung email");
         log.info(message.toString());
         mailSender.send(message); // Gọi lệnh gửi
+
+        template.opsForValue().set("loda","hello world");
+
+        // In ra màn hình Giá trị của key "loda" trong Redis
+        System.out.println("Value of key loda: "+template.opsForValue().get("loda"));
+
     }
 }
