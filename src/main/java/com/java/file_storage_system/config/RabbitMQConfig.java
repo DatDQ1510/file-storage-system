e96d0a9f2247c407227051790fb588723185509e
DatDQ5275 <datdinh5275@gmail.com>
2026-03-26 00:14:56 +0700

    Create Config RabbitMQ

---
+++ src/main/java/com/java/file_storage_system/config/RabbitMQConfig.java
@@ -0,0 +1,9 @@
+package com.java.file_storage_system.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class RabbitMQConfig {
+}
